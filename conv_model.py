a1da8a853c81b2f900a52ac5ce12af46de85472e
Camil Bouzidi, B. Eng <camilbouzidi@hotmail.com>
2023-11-26 20:00:44 -0500

    added CNN and guesser overhaul

---
+++ conv_model.py
@@ -0,0 +1,154 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+# Here, we implement a model that has different layers and optimizer.
+# It's a Convolutional Neural Network, we thus expect a better accuracy than the linear Neural Network
+# implemented in model.py
+
+# Hyperparameters
+learning_rate = 1e-3
+batch_size = 4
+epochs = 1000
+
+class ConvNeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Each image has 3 color channels
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class ConvNeuralNetworkManager:
+    def __init__(self, device):
+        self.device = device
+    
+    def training_loop(self, dataloader, model, loss_fn, optimizer):
+        size = len(dataloader.dataset)
+        # training mode
+        model.train()
+
+        # let's iterate over the dataset twice
+        for epoch in range(2):
+            # keep track of the running loss
+            running_loss = 0.0
+            for batch, (X, y) in enumerate(dataloader, 0):
+                X, y = X.to(self.device), y.to(self.device)  # Move data to the correct device
+
+                # zero parameter gradients
+                optimizer.zero_grad()
+                
+                # prediction and loss
+                pred = model(X)
+                loss = loss_fn(pred, y)
+
+                # backpropagation without zeroing the parameter gradients
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+                if batch % 2000 == 1999:    # print every 2000 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+        print(f"Finished training across {epoch+1} epochs.")
+    
+
+    def testing_loop(self, dataloader, model, loss_fn):
+        # evaluation mode
+        model.eval()
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+
+        # Evaluate model (no gradients mode for memory usage purposes)
+        with torch.no_grad():
+            for X, y in dataloader:
+                X, y = X.to(self.device), y.to(self.device)  # Move data to the correct device
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        
+    # Script to run and optimize the model:
+    def run_and_optimize(self):
+        # exit if GPU not used
+        if not torch.cuda.is_available():
+            print("cuda not available")
+            print(f"{torch.cuda.get_device_name(0)}")
+            return
+        
+        print("cude available")
+        print(f"{torch.cuda.get_device_name(0)}")
+
+        # Use tensors of normalized range [-1,1]
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        # Generate training and test data
+        training_data = datasets.CIFAR10(
+            root='./data', 
+            train=True,
+            download=True,
+            transform=transform
+        )
+
+        testing_data = datasets.CIFAR10(
+            root='./data', 
+            train=False,
+            download=True,
+            transform=transform
+        )
+
+        # use the dataloader for both training and testing data
+        training_dataloader = DataLoader(
+            training_data,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2
+        )
+
+        testing_dataloader = DataLoader(
+            testing_data,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=2
+        )
+
+        classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+        # Create the model using the selected device.
+        model = ConvNeuralNetwork().to(self.device)
+        print(model)
+
+        # Initialize the loss function
+        loss_fn = nn.CrossEntropyLoss()
+
+        # Optimizer with momentum
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+        for t in range(epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            self.training_loop(training_dataloader, model, loss_fn, optimizer)
+            self.testing_loop(testing_dataloader, model, loss_fn)
+        print("Done!")
+
+        # Save the model
+        torch.save(model.state_dict(), 'conv_model_weights.pth')
--- guesser.py
+++ guesser.py
@@ -3,14 +3,11 @@ from PIL import Image
 import torchvision.transforms as transforms
 import torchvision.models as models
 from model import NeuralNetwork, NeuralNetworkManager
+from conv_model import ConvNeuralNetwork, ConvNeuralNetworkManager
 import torchvision.datasets as datasets
 
-# Current accuracy 48.3%
-# Hyperparameters
-# learning_rate = 1e-3
-# batch_size = 512
-# epochs = 1000
-retrain_model = False
+retrain_model = True
+use_convNN = True
 
 device = (
             "cuda"
@@ -22,50 +19,104 @@ device = (
 print(f"Using {device} device")
 
 
+def use_linear_model():
+    # Current accuracy with model.py: 48.3%
+    # Hyperparameters
+    # learning_rate = 1e-3
+    # batch_size = 512
+    # epochs = 1000
 
-# Optional: Retrain the model
-if retrain_model:
-    manager = NeuralNetworkManager(device)
-    manager.run_and_optimize()
+    # Optional: Retrain the model
+    if retrain_model:
+        manager = NeuralNetworkManager(device)
+        manager.run_and_optimize()
 
 
-# Load the trained model
-model = NeuralNetwork()
-state_dict = torch.load('C:/GitHub/PyTorch-Exercise/model_weights.pth')
-model.load_state_dict(state_dict)
+    # Load the trained model
+    model = NeuralNetwork()
+    state_dict = torch.load('C:/GitHub/PyTorch-Exercise/model_weights.pth')
+    model.load_state_dict(state_dict)
 
-# Set the model to evaluation mode
-model.eval()
+    # Set the model to evaluation mode
+    model.eval()
 
-# Create the input
-# Load the image
-img = Image.open('C:/GitHub/PyTorch-Exercise/horse.jpg')
+    # Create the input
+    # Load the image
+    img = Image.open('C:/GitHub/PyTorch-Exercise/input3.jpg')
 
-# Resize the image to 32x32 pixels
-img = img.resize((32, 32))
+    # Resize the image to 32x32 pixels
+    img = img.resize((32, 32))
 
-# Convert the image to a PyTorch tensor
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
+    # Convert the image to a PyTorch tensor
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
 
-# unsqueeze needed because of batch size 1
-input_tensor = transform(img).unsqueeze(0)
-print(input_tensor.size())
+    # unsqueeze needed because of batch size 1
+    input_tensor = transform(img).unsqueeze(0)
+    print(input_tensor.size())
 
-# Pass the input tensor to the model
-output = model(input_tensor)
+    # Pass the input tensor to the model
+    output = model(input_tensor)
 
-# Get the predicted class
-_, predicted = torch.max(output.data, 1)
+    # Get the predicted class
+    _, predicted = torch.max(output.data, 1)
 
-# Load the CIFAR-10 dataset
-cifar10_dataset = datasets.CIFAR10(root='./data', train=True, download=True)
+    # Load the CIFAR-10 dataset
+    cifar10_dataset = datasets.CIFAR10(root='./data', train=True, download=True)
 
-# Get the list of class labels
-class_labels = cifar10_dataset.classes
+    # Get the list of class labels
+    class_labels = cifar10_dataset.classes
 
-# Now you can get the label of the predicted class
-predicted_label = class_labels[predicted.item()]
+    # Now you can get the label of the predicted class
+    predicted_label = class_labels[predicted.item()]
 
-print(predicted_label)
+    print(predicted_label)
+
+def use_conv_model():
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = 4
+    if retrain_model:
+        manager = ConvNeuralNetworkManager(device)
+        manager.run_and_optimize()
+    
+    # Load the trained model
+    model = ConvNeuralNetwork()
+    state_dict = torch.load('C:/GitHub/PyTorch-Exercise/conv_model_weights.pth')
+    model.load_state_dict(state_dict)
+
+    # Set the model to evaluation mode
+    model.eval()
+
+    # Create the input
+    # Load the image
+    img = Image.open('C:/GitHub/PyTorch-Exercise/input3.jpg')
+
+    # Resize the image to 32x32 pixels
+    img = img.resize((32, 32))
+    # unsqueeze needed because of batch size 1
+    input_tensor = transform(img).unsqueeze(0)
+    print(input_tensor.size())
+
+    # Pass the input tensor to the model
+    output = model(input_tensor)
+
+    # Get the predicted class
+    _, predicted = torch.max(output.data, 1)
+
+    # Load the CIFAR-10 dataset
+    cifar10_dataset = datasets.CIFAR10(root='./data', train=True, download=True)
+
+    # Get the list of class labels
+    class_labels = cifar10_dataset.classes
+
+    # Now you can get the label of the predicted class
+    predicted_label = class_labels[predicted.item()]
+    print(predicted_label)
+
+
+if use_convNN:
+    use_conv_model()
+else:
+    use_linear_model()
+    
